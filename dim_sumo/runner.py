39df5b5a3898b8fd6bacbbaf671e3102fad17041
sdelgadom <sdelgadom@unal.edu.co>
2021-12-28 20:52:42 -0500

    scripting route generator for different city sizes

--- dim_sumo/runner.py
+++ dim_sumo/runner.py
@@ -42,8 +42,18 @@ else:
 from sumolib import checkBinary  # noqa
 import traci  # noqa
 
+def get_node_number(i, j, city_size):
+    if (i == 0 and j == 0) or (i == 0 and j == (city_size + 1)) or (i == (city_size + 1) and j == 0) or (
+            i == (city_size + 1) and j == (city_size + 1)):
+        return -1
+    if i == 0 or i == (city_size + 1):
+        node = city_size * (city_size + 2) + 2 * (j - 1) + 1 + (i != 0)
+    else:
+        node = (i - 1) * (city_size + 2) + 1 + j
+    return node
+
 def generate_routefile(seconds = 3600, pWE = 0.1, pNS = 0.1, pSN=0.1, pEW=0.1, dWE = 0.1, dNS = 0.0, pEmergency = 0.01,
-                       routefile=None):
+                       routefile=None, city_size=2):
     """ Generates a route file with the level of traffic described by the parameters
 
     Args:
@@ -65,14 +75,62 @@ def generate_routefile(seconds = 3600, pWE = 0.1, pNS = 0.1, pSN=0.1, pEW=0.1, d
 
     with open(routefile, "w") as routes:
         print(f"""<routes>
-        <vType id="Car" length="5.00" minGap="2.50" maxSpeed="16.67" guiShape="passenger" carFollowModel="IDM" accel="0.8" decel="4.5" tau="1.0"/>
+      <vType id="Car" length="5.00" minGap="2.50" maxSpeed="16.67" guiShape="passenger" carFollowModel="IDM" accel="0.8" decel="4.5" tau="1.0"/>
+            """, file=routes)
+        for i in range(1, city_size+1, 2): #Right routes
+            id_route = str(int((i+1)/2))
+            route = "    <route id=\"right" + id_route + "\" "
+            edges = []
+            for j in range(city_size):
+                node_from = get_node_number(i, j, city_size)
+                node_to = get_node_number(i, j+1, city_size)
+                edges.append(str(node_from) + "to" + str(node_to))
+            edges.append("outr" + id_route)
+            route += " edges=\"" + " ".join(edges) + "\" />"
+            print(route, file=routes)
 
-        <route id="right" edges="1to2 2to3 outr" />
-        <route id="down" edges="9to2 2to6 outs" />
-        <route id="up" edges="12to7 7to3 outn" />
-        <route id="left" edges="8to7 7to6 outl" />
-        """, file=routes)
-        vehNr = 0
+        for i in range(2, city_size+1, 2): #Left routes
+            id_route = str(int((i)/2))
+            route = "    <route id=\"left" + id_route + "\" "
+            edges = []
+            for j in range(city_size, 0, -1):
+
+                node_from = get_node_number(i, j + 1, city_size)
+                node_to = get_node_number(i, j, city_size)
+                edges.append(str(node_from) + "to" + str(node_to))
+            edges.append("outl" + id_route)
+            route += " edges=\"" + " ".join(edges) + "\" />"
+            print(route, file=routes)
+
+
+        for j in range(1, city_size+1, 2): #Down routes
+            id_route = str(int((j+1)/2))
+            route = "    <route id=\"down" + id_route + "\" "
+            edges = []
+            for i in range(city_size):
+                node_from = get_node_number(i, j, city_size)
+                node_to = get_node_number(i + 1, j, city_size)
+
+                edges.append(str(node_from) + "to" + str(node_to))
+            edges.append("outs" + id_route)
+            route += " edges=\"" + " ".join(edges) + "\" />"
+            print(route, file=routes)
+
+        for j in range(2, city_size+1, 2): #Up routes
+            id_route = str(int((j)/2))
+            route = "    <route id=\"up" + id_route + "\" "
+            edges = []
+            for i in range(city_size, 0, -1):
+
+                node_from = get_node_number(i + 1, j, city_size)
+                node_to = get_node_number(i, j, city_size)
+                edges.append(str(node_from) + "to" + str(node_to))
+            edges.append("outn" + id_route)
+            route += " edges=\"" + " ".join(edges) + "\" />"
+            print(route, file=routes)
+
+        """
+        
         veh_der = 0
         veh_izq = 0
         veh_aba = 0
@@ -83,8 +141,25 @@ def generate_routefile(seconds = 3600, pWE = 0.1, pNS = 0.1, pSN=0.1, pEW=0.1, d
             "left": veh_izq,
             "up": veh_arr,
             "down": veh_aba
-        }
+        }Nr
         routes_list = ["right", "left", "up", "down"]
+        """
+        vehNr = 0
+        veh_emergency = 0
+        routes_list = []
+
+        mid_size = int((city_size+1)/2)
+        for i in range(mid_size):
+            routes_list.append("right" + str(i+1))
+            routes_list.append("down" + str(i + 1))
+        for i in range(city_size - mid_size):
+            routes_list.append("left" + str(i+1))
+            routes_list.append("up" + str(i + 1))
+
+        dict_routes = {}
+        for r in routes_list:
+            dict_routes[r] = 0
+
 
         for i in range(seconds):
 
@@ -97,42 +172,51 @@ def generate_routefile(seconds = 3600, pWE = 0.1, pNS = 0.1, pSN=0.1, pEW=0.1, d
                 veh_emergency += 1
                 dict_routes[decision_route] += 1
 
-            if random.rand()< pWE:
-                color = "yellow"
-                print(f'    <vehicle id="right_%i" type="Car" color="{color}" route="right" departSpeed="{depart_speed}" depart="%i" />' % (vehNr, i), file=routes)
-                vehNr += 1
-                veh_der+=1
+            for j in range(mid_size):
+                if random.rand()< pWE:
+                    color = "yellow"
+                    decision_route = "right" + str(j+1)
+                    print(f'    <vehicle id="{decision_route}_%i" type="Car" color="{color}" route="{decision_route}" departSpeed="{depart_speed}" depart="%i" />' % (vehNr, i), file=routes)
+                    vehNr += 1
+                    dict_routes[decision_route] += 1
+
+            for j in range(mid_size):
+                if random.rand() < pNS:
+                    color = "yellow"
+                    decision_route = "down" + str(j + 1)
+                    print(f'    <vehicle id="{decision_route}_%i" type="Car" color="{color}" route="{decision_route}" departSpeed="{depart_speed}" depart="%i" />' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    dict_routes[decision_route] += 1
+
+            for j in range(city_size - mid_size):
+                if random.rand() < pSN:
+                    color = "yellow"
+                    decision_route = "up" + str(j + 1)
+                    print(f'    <vehicle id="{decision_route}_%i" type="Car" color="{color}" route="{decision_route}" departSpeed="{depart_speed}" depart="%i" />' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    dict_routes[decision_route] += 1
+
+            for j in range(city_size - mid_size):
+                if random.rand() < pEW:
+                    color = "yellow"
+                    decision_route = "left" + str(j + 1)
+                    print(f'    <vehicle id="{decision_route}_%i" type="Car" color="{color}" route="{decision_route}" departSpeed="{depart_speed}" depart="%i" />' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    dict_routes[decision_route] += 1
 
 
-            if random.rand() < pNS:
-                color = "yellow"
-                print(f'    <vehicle id="down_%i" type="Car" color="{color}" route="down" departSpeed="{depart_speed}" depart="%i" />' % (
-                    vehNr, i), file=routes)
-                vehNr += 1
-                veh_arr += 1
+        for r in routes_list:
+            print(" Vehiculos por la ruta: ", r, dict_routes[r])
 
+        #print(" Vehiculos por la izquierda ", veh_izq)
 
-            if random.rand() < pSN:
-                color = "yellow"
-                print(f'    <vehicle id="up_%i" type="Car" color="{color}" route="up" departSpeed="{depart_speed}" depart="%i" />' % (
-                    vehNr, i), file=routes)
-                vehNr += 1
-                veh_aba += 1
-
-
-            if random.rand() < pEW:
-                color = "yellow"
-                print(f'    <vehicle id="left_%i" type="Car" color="{color}" route="left" departSpeed="{depart_speed}" depart="%i" />' % (
-                    vehNr, i), file=routes)
-                vehNr += 1
-                veh_izq += 1
-
-
-        print(" Vehiculos por la izquierda ", veh_izq)
-
-        print(" Vehiculos por la derecha ", veh_der)
-        print(" Vehiculos por la arriba ", veh_arr)
-        print(" Vehiculos por la abajo ", veh_aba)
+        #print(" Vehiculos por la derecha ", veh_der)
+        #print(" Vehiculos por la arriba ", veh_arr)
+        #print(" Vehiculos por la abajo ", veh_aba)
+        print(" Vehiculos totales ", vehNr)
         print(" Vehiculos emergencia ", veh_emergency)
         print("</routes>", file=routes)
 
@@ -145,10 +229,18 @@ def generate_routefile(seconds = 3600, pWE = 0.1, pNS = 0.1, pSN=0.1, pEW=0.1, d
 #        <phase duration="6"  state="ryry"/>
 #    </tlLogic>
 
-def run(traffic_lights=False):
+def run(traffic_lights=False, trafficlights_flaws=0.25):
     """execute the TraCI control loop"""
     state = Simulation()
+    total_trafficlights = list(traci.trafficlight.getIDList())
+    print(total_trafficlights)
+    #import random as rn
+    #flaw_trafficlights = rn.choice(total_trafficlights, max(int(len(total_trafficlights)*trafficlights_flaws), 1))
 
+    #for id in flaw_trafficlights:
+    #    new_phase = rn.choice([0, 2])
+    #    print("al semaforo", id, " lo vamos poner en fase ", new_phase)
+    #    traci.trafficlight.setPhase(id, new_phase)
     step = 0
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
@@ -207,13 +299,17 @@ def get_options():
     return options
 
 def generate_traffic_and_execute_sumo(sumoBinary, output_path, pWE = 0.1, pNS = 0.1, dWE = 0.1, pEW=0.1, pSN=0.1,
-                                      dNS = 0.0, pEmergency=0.01,traffic_lights=False):
+                                      dNS = 0.0, pEmergency=0.01,traffic_lights=False, trafficlights_flaws=0.25, city_size=2):
 
-    routefile= "data/ciudad2x2_semaforo.rou.xml" if traffic_lights else "data/cross.rou.xml"
+    name_according_city = str(city_size) + "x" + str(city_size)
+    routefile= "data/ciudad" + name_according_city + "_semaforo.rou.xml" if traffic_lights else "data/cross"+ name_according_city + ".rou.xml"
     # first, generate the route file for this simulation
-    generate_routefile(pWE=pWE, pNS=pNS, pEW=pEW, pSN=pSN, dWE=dWE, dNS=dNS, pEmergency=pEmergency, routefile=routefile)
+    print(routefile)
+    generate_routefile(pWE=pWE, pNS=pNS, pEW=pEW, pSN=pSN, dWE=dWE, dNS=dNS, pEmergency=pEmergency, routefile=routefile, city_size=city_size)
 
-    cfg_sumo_file = "data/ciudad2x2_semaforo.sumocfg" if traffic_lights else "data/cross.sumocfg"
+    cfg_sumo_file = f"data/ciudad{city_size}x{city_size}_semaforo.sumocfg" if traffic_lights else f"data/cross{city_size}x{city_size}.sumocfg"
+
+
 
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
@@ -221,7 +317,7 @@ def generate_traffic_and_execute_sumo(sumoBinary, output_path, pWE = 0.1, pNS = 
                 "--collision.mingap-factor", "0",
                 "--step-length", "0.2",
                 "--tripinfo-output", output_path])
-    run(traffic_lights)
+    run(traffic_lights, trafficlights_flaws)
 
 
 def run_batch(sumoBinary, vph_combinations, dec_array, traffic_lights):
@@ -237,7 +333,7 @@ def run_batch(sumoBinary, vph_combinations, dec_array, traffic_lights):
                 print(file)
                 generate_traffic_and_execute_sumo(sumoBinary, file, pWE=vph[1]/3600, pNS=vph[0]/3600, pSN=vph[0]/3600,
                                                   pEW=vph[0] / 3600, dWE=we_dec, dNS=ns_dec, pEmergency=0.01,
-                                                  traffic_lights=traffic_lights)
+                                                  traffic_lights=traffic_lights, city_size=5)
 
 
 def main(options = None):
@@ -256,8 +352,8 @@ def main(options = None):
     else:
         sumoBinary = checkBinary('sumo-gui')
     
-    generate_traffic_and_execute_sumo(sumoBinary, "data/out-tripinfo.xml", dNS=0.0, dWE=0.0, pNS=720/3600, pWE=720/3600,
-                                      pSN=720/3600, pEW=720/3600, pEmergency=0.01, traffic_lights=True)
+    generate_traffic_and_execute_sumo(sumoBinary, "data/out-tripinfo.xml", dNS=0.0, dWE=0.0, pNS=3600/3600, pWE=3600/3600,
+                                      pSN=3600/3600, pEW=3600/3600, pEmergency=0.01, traffic_lights=False, trafficlights_flaws=0.25, city_size=10)
 
 # this is the main entry point of this script
 if __name__ == "__main__":
